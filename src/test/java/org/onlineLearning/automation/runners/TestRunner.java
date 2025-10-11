98159148acbad234c3e8aefe41bc910c8ab2c6b9
pratikgole95 <pratikgole95@gmail.com>
2025-10-11 23:12:27 +0530

    [TM-111025]update test runner file

--- src/test/java/org/onlineLearning/automation/runners/TestRunner.java
+++ src/test/java/org/onlineLearning/automation/runners/TestRunner.java
@@ -3,13 +3,18 @@ package org.onlineLearning.automation.runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = {"org.onlineLearning.automation.stepDefinitions"},
-        plugin = {"pretty","html:target/cucumber-reports.html","json:target/cucumber.json"},
+        glue = {
+                "org.onlineLearning.automation.stepDefinitions",
+                "org.onlineLearning.automation.hooks"
+        },
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports.html",
+                "json:target/cucumber.json"
+        },
         monochrome = true
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
-
